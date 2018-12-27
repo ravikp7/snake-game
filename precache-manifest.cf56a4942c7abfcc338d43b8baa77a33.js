df03fd59b18a625d534aece9d89e1371230ccdda
Ravi Kumar Prasad <7ravikp@gmail.com>
2018-12-27 21:55:55 +0530

    Updates

---
+++ precache-manifest.cf56a4942c7abfcc338d43b8baa77a33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "351fc99354337a05b0be",
+    "url": "/snake-game/static/js/runtime~main.351fc993.js"
+  },
+  {
+    "revision": "b2221ac2ad24deecd4bb",
+    "url": "/snake-game/static/js/main.b2221ac2.chunk.js"
+  },
+  {
+    "revision": "b67c2534f6b726dc7a02",
+    "url": "/snake-game/static/js/1.b67c2534.chunk.js"
+  },
+  {
+    "revision": "b2221ac2ad24deecd4bb",
+    "url": "/snake-game/static/css/main.48773eda.chunk.css"
+  },
+  {
+    "revision": "56f6c6b3383bf99771aa3f4a874dca66",
+    "url": "/snake-game/index.html"
+  }
+];
